30813bc6949b75f72b02c3c1e3d3a577b45a6127
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-05-06 13:15:57 -0700

    create Stylist class and object. Pass unit test for Stylist object.

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,55 +2,55 @@ import org.sql2o.*;
 import java.util.Arrays;
 import java.util.List;
 
-// public class Cuisine {
-//   private int id;
-//   private String cuisine_type;
-//
-//   public Cuisine(String cuisine_type) {
-//     this.cuisine_type = cuisine_type;
-//   }
+public class Stylist {
+  private int id;
+  private String name;
+
+  public Stylist(String name) {
+    this.name = name;
+  }
 //
 //   public String getType() {
-//     return cuisine_type;
+//     return name;
 //   }
 //
 //   public int getId() {
 //     return id;
 //   }
 //
-//   public static List<Cuisine> all() {
-//     String sql = "SELECT id, cuisine_type FROM cuisines";
+//   public static List<Stylist> all() {
+//     String sql = "SELECT id, name FROM cuisines";
 //     try(Connection con = DB.sql2o.open()) {
-//       return con.createQuery(sql).executeAndFetch(Cuisine.class);
+//       return con.createQuery(sql).executeAndFetch(Stylist.class);
 //     }
 //   }
 //
 //   @Override
-//   public boolean equals(Object otherCuisine) {
-//     if (!(otherCuisine instanceof Cuisine)) {
+//   public boolean equals(Object otherStylist) {
+//     if (!(otherStylist instanceof Stylist)) {
 //       return false;
 //     } else {
-//       Cuisine newCuisine = (Cuisine) otherCuisine;
-//       return this.getType().equals(newCuisine.getType());
+//       Stylist newStylist = (Stylist) otherStylist;
+//       return this.getType().equals(newStylist.getType());
 //     }
 //   }
 //
 //   public void save() {
 //     try(Connection con = DB.sql2o.open()) {
-//       String sql = "INSERT INTO cuisines(cuisine_type) VALUES (:cuisine_type)";
+//       String sql = "INSERT INTO cuisines(name) VALUES (:name)";
 //       this.id = (int) con.createQuery(sql, true)
-//         .addParameter("cuisine_type", this.cuisine_type)
+//         .addParameter("name", this.name)
 //         .executeUpdate()
 //         .getKey();
 //     }
 //   }
 //
-//   public static Cuisine find(int id) {
+//   public static Stylist find(int id) {
 //     try(Connection con = DB.sql2o.open()) {
 //       String sql = "SELECT * FROM cuisines WHERE id=:id";
-//       Cuisine cuisine = con.createQuery(sql)
+//       Stylist cuisine = con.createQuery(sql)
 //         .addParameter("id", id)
-//         .executeAndFetchFirst(Cuisine.class);
+//         .executeAndFetchFirst(Stylist.class);
 //       return cuisine;
 //     }
 //   }
@@ -64,11 +64,11 @@ import java.util.List;
 //     }
 //   }
 //
-//   public void update(String cuisine_type) {
+//   public void update(String name) {
 //     try(Connection con = DB.sql2o.open()) {
-//       String sql = "UPDATE cuisines SET cuisine_type = :cuisine_type WHERE id=:id";
+//       String sql = "UPDATE cuisines SET name = :name WHERE id=:id";
 //       con.createQuery(sql)
-//         .addParameter("cuisine_type", cuisine_type)
+//         .addParameter("name", name)
 //         .addParameter("id", id)
 //         .executeUpdate();
 //     }
@@ -82,4 +82,4 @@ import java.util.List;
 //         .executeUpdate();
 //     }
 //   }
-// }
+}
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -3,84 +3,84 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
 
-// public class StylistTest {
-//
-//   @Rule
-//   public DatabaseRule database = new DatabaseRule();
-//
+public class StylistTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist testStylist = new Stylist("Rachel");
+    assertTrue(testStylist instanceof Stylist);
+  }
+
   // @Test
-  // public void Cuisine_instantiatesCorrectly_true() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   assertTrue(testCuisine instanceof Cuisine);
+  // public void Stylist_instantiatesWithStylistType_String() {
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   assertEquals("Rachel", testStylist.getType());
   // }
   //
   // @Test
-  // public void Cuisine_instantiatesWithCuisineType_String() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   assertEquals("BBQ", testCuisine.getType());
-  // }
-  //
-  // @Test
-  // public void all_CuisineListEmptyAtFirst() {
-  //   assertEquals(Cuisine.all().size(), 0);
+  // public void all_StylistListEmptyAtFirst() {
+  //   assertEquals(Stylist.all().size(), 0);
   // }
   //
   // @Test
   // public void equals_returnsTrueIfNamesAreSame() {
-  //   Cuisine firstCuisine = new Cuisine("BBQ");
-  //   Cuisine secondCuisine = new Cuisine("BBQ");
-  //   assertTrue(firstCuisine.equals(secondCuisine));
+  //   Stylist firstStylist = new Stylist("Rachel");
+  //   Stylist secondStylist = new Stylist("Rachel");
+  //   assertTrue(firstStylist.equals(secondStylist));
   // }
   //
   // @Test
   // public void save_savesIntoDatabase_true() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   assertTrue(Cuisine.all().get(0).equals(testCuisine));
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   assertTrue(Stylist.all().get(0).equals(testStylist));
   // }
   //
   // @Test
   // public void save_assignsIdToObject() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   Cuisine savedCuisine = Cuisine.all().get(0);
-  //   assertEquals(testCuisine.getId(), savedCuisine.getId());
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   Stylist savedStylist = Stylist.all().get(0);
+  //   assertEquals(testStylist.getId(), savedStylist.getId());
   // }
   //
   // @Test
-  // public void find_findCuisineInDatabase_true() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   Cuisine savedCuisine = Cuisine.find(testCuisine.getId());
-  //   assertTrue(testCuisine.equals(savedCuisine));
+  // public void find_findStylistInDatabase_true() {
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   Stylist savedStylist = Stylist.find(testStylist.getId());
+  //   assertTrue(testStylist.equals(savedStylist));
   // }
   //
   // @Test
   // public void getRestaurants_retrievesAllRestaurantsFromDatabase_restaurantsList() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   Restaurant firstRestaurant = new Restaurant("Pit BBQ", "Tasty smoke", testCuisine.getId());
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   Restaurant firstRestaurant = new Restaurant("Pit Rachel", "Tasty smoke", testStylist.getId());
   //   firstRestaurant.save();
-  //   Restaurant secondRestaurant = new Restaurant("Sticky Fingers", "Saucy sauce", testCuisine.getId());
+  //   Restaurant secondRestaurant = new Restaurant("Sticky Fingers", "Saucy sauce", testStylist.getId());
   //   secondRestaurant.save();
   //   Restaurant[] restaurants = new Restaurant[] { firstRestaurant, secondRestaurant };
-  //   assertTrue(testCuisine.getRestaurants().containsAll(Arrays.asList(restaurants)));
+  //   assertTrue(testStylist.getRestaurants().containsAll(Arrays.asList(restaurants)));
   // }
   //
   // @Test
-  // public void update_updatesCuisineType_true() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   testCuisine.update("Texas BBQ");
-  //   assertEquals("Texas BBQ", Cuisine.find(testCuisine.getId()).getType());
+  // public void update_updatesStylistType_true() {
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   testStylist.update("Texas Rachel");
+  //   assertEquals("Texas Rachel", Stylist.find(testStylist.getId()).getType());
   // }
   //
   // @Test
-  // public void delete_deletesCuisineType_true() {
-  //   Cuisine testCuisine = new Cuisine("BBQ");
-  //   testCuisine.save();
-  //   int testCuisineId = testCuisine.getId();
-  //   testCuisine.delete();
-  //   assertEquals(null, Cuisine.find(testCuisineId));
+  // public void delete_deletesStylistType_true() {
+  //   Stylist testStylist = new Stylist("Rachel");
+  //   testStylist.save();
+  //   int testStylistId = testStylist.getId();
+  //   testStylist.delete();
+  //   assertEquals(null, Stylist.find(testStylistId));
   // }
-// }
+}
